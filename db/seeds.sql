fc568e4fabe1ef4ca2e6f98b2a001b7bd8f391fa
Amir <amir.hackett@gmail.com>
2022-05-09 16:13:03 -0400

    updated seeds file and package.json

--- db/seeds.sql
+++ db/seeds.sql
@@ -20,10 +20,10 @@ INSERT INTO role (title, salary, department_id)
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
     VALUES 
     ('Mark', 'Miller', 2, null),
-    ('Devin', 'Anderson', 1, 1),
+    ('Roland', 'Deschain', 1, 1),
     ('Mary', 'Brown', 4, null),
     ('Ashley', 'Jones', 3, 3),
     ('Tyler', 'Moore', 6, null),
     ('Ana', 'Sanchez', 5, 5),
-    ('Lewis', 'Allen', 7, null),
+    ('Lewis', 'Hamilton', 7, null),
     ('Katherine', 'Green', 8, 7);

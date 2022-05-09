fb5a4579aa0beb3661e71b7feaf39b72d501f332
Amir <amir.hackett@gmail.com>
2022-05-09 16:19:15 -0400

    updated schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -3,27 +3,24 @@ DROP TABLE IF EXISTS role
 DROP TABLE IF EXISTS employee
 
 CREATE TABLE department (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL
+    id INTEGER NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER NOT NULL,
     title VARCHAR(30) NOT NULL, 
-    salary DECIMAL NOT NULL,
-    department_id INTEGER, 
-    INDEX dep_ind (department_id),
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT UNSIGNED NOT NULL REFERENCES departments(id),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER, 
-    INDEX role_ind (role_id),
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-    manager_id INTEGER,
-    INDEX manager_ind (manager_id),
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+  id INTEGER NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER UNSIGNED NOT NULL REFERENCES roles(id),
+  manager_id INTEGER UNSIGNED REFERENCES manager(id),
+  PRIMARY KEY (id)
 );

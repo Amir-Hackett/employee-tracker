74526a60e29b5ebecd77c8393567adcb8ad2a3e4
Amir <amir.hackett@gmail.com>
2022-05-13 12:50:23 -0400

    added chaulk and figlet

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 const db = require('./db/connection');
 const inquirer = require('inquirer'); 
 const cTable = require('console.table'); 
+const chalk = require('chalk');
+const figlet = require('figlet');

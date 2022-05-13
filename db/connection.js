aa87ad16e2bae11869e8df834a4a66784245b7d7
Amir <amir.hackett@gmail.com>
2022-05-13 15:41:19 -0400

    added functionality to server.js

--- db/connection.js
+++ db/connection.js
@@ -2,16 +2,15 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 // connect to database
-const db = mysql.createConnection({
-
-  host: 'localhost',
+const db = {
+  host: '127.0.0.1',
     // Your MySQL username
     user: process.env.DB_USER,
     // Your MySQL password
     password: process.env.DB_PASSWORD,
     database: "employee_db"
-},
-console.log('Connected to the employee database')
-);
+}
 
-module.exports = db;
+const connection = mysql.createConnection(db);
+
+module.exports = db, connection

2d7ae037bda161a5d396c7cb88794999219ed631
Amir <amir.hackett@gmail.com>
2022-05-13 12:38:57 -0400

    added dotenv

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,17 @@
-const mysql = require("mysql2");
+const mysql = require('mysql2');
+require('dotenv').config();
 
-// Connect to database
+// connect to database
 const db = mysql.createConnection({
-    host: 'localhost',
-    // Your MySQL username,
-    user: 'root',
+
+  host: 'localhost',
+    // Your MySQL username
+    user: process.env.DB_USER,
     // Your MySQL password
-    password: 'Ami0799!',
-    database: 'employee_db'
-  },
-  console.log('Connected to the election database.')
+    password: process.env.DB_PASSWORD,
+    database: "employee_db"
+},
+console.log('Connected to the employee database')
 );
 
 module.exports = db;
